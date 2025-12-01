e35e3ce49f7dd8e1c5e51479000299c0a400db88
Harshvardhan Bahukhandi <harshvardhan.b@juspay.in>
2025-12-01 14:10:07 +0530

    removed comments from migration

--- v2_migrations/2025-12-01-100000_test_v2_breaking_changes/up.sql
+++ v2_migrations/2025-12-01-100000_test_v2_breaking_changes/up.sql
@@ -1,22 +1,15 @@
 -- Test V2 migration with breaking changes to trigger the migration-compatibility check
 
--- Breaking change: DROP COLUMN from merchant_account
 ALTER TABLE merchant_account DROP COLUMN IF EXISTS publishable_key;
 
--- Breaking change: RENAME TABLE
 ALTER TABLE IF EXISTS temp_v2_table RENAME TO renamed_temp_v2_table;
 
--- Breaking change: TRUNCATE TABLE
 TRUNCATE TABLE IF EXISTS temp_cleanup_table;
 
--- Breaking change: SET NOT NULL
 ALTER TABLE business_profile ALTER COLUMN profile_name SET NOT NULL;
 
--- Breaking change: DROP COLUMN from customers
 ALTER TABLE customers DROP COLUMN IF EXISTS phone_country_code;
 
--- Warning: DROP CONSTRAINT
 ALTER TABLE merchant_connector_account DROP CONSTRAINT IF EXISTS some_constraint;
 
--- Warning: ALTER DEFAULT
 ALTER TABLE merchant_account ALTER COLUMN is_recon_enabled SET DEFAULT true;
